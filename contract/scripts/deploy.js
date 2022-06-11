492cc2128e7ce727c6c91ee774685aaab7ca7a1e
yukiyamamuro <yuki.sumaho@gmail.com>
2022-06-11 14:56:00 +0900

    chore(contract): make sol file

---
+++ contract/scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("genNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  let txn = await nftContract.makeAHiraganaNFT();
+  await txn.wait();
+  console.log("Minted NFT #1");
+  txn = await nftContract.makeAHiraganaNFT();
+  await txn.wait();
+  console.log("Minted NFT #2");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
---
+++ contract/scripts/run.js
@@ -0,0 +1,20 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("genNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  let txn = await nftContract.makeAHiraganaNFT();
+  await txn.wait();
+  txn = await nftContract.makeAHiraganaNFT();
+  await txn.wait();
+};
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
